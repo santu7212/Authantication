808ef04ba9910d351044ce30128a4778f5ba329d
santu7212 <santupramanik540@gmail.com>
2025-11-20 19:23:43 +0530

    create user schema

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,2 +1,17 @@
 
 
+// step 1 cretae user 
+
+const createUser=async(req,res)=>{
+    try {
+        const {username,email,password}=req.body
+        
+        
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({success:false,message:error.message})
+        
+        
+    }
+}
+
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -4,11 +4,32 @@ const userSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, trim: true },
     email: { type: String, required: true, unique: true },
-    password:{type:String,required:true}
+    password: { type: String, required: true },
+    verifyOtp: {
+      type: String,
+      default: "",
+    },
+    verifyOtpExpiresAt: {
+      type: Number,
+      default: 0,
+    },
+    isAccountVerified: {
+      type: Boolean,
+      default: false,
+    },
+    resteOtp: {
+      type: String,
+      default: "",
+    },
+    resteOtpExpiresAt: {
+      type: Number,
+      default: 0,
+    },
   },
+
   { timestamps: true }
 );
 
-const User=mongoose.model("User",userSchema)
+const User = mongoose.model("User", userSchema);
 
-export default User
+export default User;
