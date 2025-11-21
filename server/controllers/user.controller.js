96b0dd039503a9d98bb0c11166550e1722fcf0f8
santu7212 <santupramanik540@gmail.com>
2025-11-21 18:48:09 +0530

    write login function

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -38,3 +38,47 @@ const register = async (req, res) => {
     res.status(500).json({ success: false, message: error.message });
   }
 };
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      // console.log("Enter email or password");
+      return res
+        .status(400)
+        .json({ success: false, message: "Enter email or password" });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({
+          success: false,
+          message: "user with this email does not exist please liginj first",
+        });
+    }
+
+    const isPasswordMatch=await bcrypt.compare(password,user.password)
+    if(!isPasswordMatch){
+      return res.status(400).json({success:false,message:"incorrect email or password"})
+
+    }
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+    return res.status(200).json({success:true,user:{username,email,token},
+    message:"Login successfully"})
+
+    
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ success: false, message: "Fail to login" });
+  }
+};
+
+
+export {register,login}
