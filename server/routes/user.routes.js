57772a940ddc01fa0e2b02bc6edf371908ee149a
santu7212 <santupramanik540@gmail.com>
2025-11-24 19:52:07 +0530

    enpoints for send reset otp andreset password

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -4,6 +4,8 @@ import {
   login,
   logOut,
   register,
+  resetPassword,
+  sendResetOTP,
   sendVerifyOtp,
   verifyEmail,
 } from "../controllers/user.controller.js";
@@ -17,5 +19,7 @@ userRouter.post("/logout", logOut);
 userRouter.post("/send-otp", verifyJWT, sendVerifyOtp);
 userRouter.post("/verify-email", verifyJWT, verifyEmail);
 userRouter.post("/is-auth", verifyJWT, isAuthanticated);
+userRouter.post("/send-reset-otp", verifyJWT, sendResetOTP);
+userRouter.post("/reset-password", verifyJWT, resetPassword);
 
 export default userRouter;
