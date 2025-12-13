9ed7ac99f2df7ef694472ebd11480d1a76c61b22
santu7212 <santupramanik540@gmail.com>
2025-12-13 20:59:02 +0530

    fix: remove unused PORT variable and streamline server export

--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
-import "dotenv/config";
+ import "dotenv/config";
 import app from "./app.js";
 import connectDB from "./config/db.js";
 
 await connectDB();
-const PORT = process.env.PORT;
 
-app.listen(PORT, () => console.log(`Server is running at ${PORT}`));
+export default app;
