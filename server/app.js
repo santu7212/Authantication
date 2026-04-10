70b7890d081f412b17d1b66f9293158d60b2077e
santu7212 <santupramanik540@gmail.com>
2026-04-10 11:29:45 +0530

    fix: remove unnecessary whitespace in app.js import statements

--- server/app.js
+++ server/app.js
@@ -1,4 +1,4 @@
- import express from "express";
+import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.routes.js";
